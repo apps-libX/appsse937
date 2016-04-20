c49d032fedfc97f57e25262bf9346fa2654c463a
anonymoussc <50c5ac69@opayq.com>
2016-04-21 00:12:47 +0700

    New fx_svr.first

---
+++ sse3/fx_svr.first.php
@@ -0,0 +1,22 @@
+<?php
+
+header('Content-Type: text/event-stream');
+
+while (true) {
+
+    $sleepSecs = mt_rand(250, 500) / 1000.0;
+
+    usleep($sleepSecs * 1000000);
+
+    $d = [
+        'timestamp' => gmdate('Y-m-d H:i:s'),
+        'symbol'    => 'GBP/USD',
+        'bid'       => 1.203,
+        'ask'       => 1.204,
+    ];
+
+    echo 'data:' . json_encode($d) . "\n\n";
+
+    @ob_flush();
+    @flush();
+}
